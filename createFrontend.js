bbe45e5ce3af8d377b4e3a4ee002bda1039b8879
Dhruv <dhruvdabhi101@gmail.com>
2024-01-27 18:33:47 +0530

    perf: increased accuracy of prompt.
    Co-authored-by: Rushi Gandhi rushigandhi14@gmail.com

--- createFrontend.js
+++ createFrontend.js
@@ -1,6 +1,6 @@
 import inquirer from "inquirer";
 import chalk from "chalk";
-import { executeOptimizations, executeSecurityChecks } from "./commands.js";
+import { executeOptimizations} from "./commands.js";
 import { filePrompt } from "./filePrompt.js";
 
 export default async function createFrontend() {
@@ -9,26 +9,17 @@ export default async function createFrontend() {
             message: chalk.blue("Which framework do you want to use?"),
             name: "Action",
             type: "list",
-            choices: ["Optimize", "Error Check", "Security Check"],
+            choices: ["Optimize", "Cancel"],
         },
     ];
 
     const results = await inquirer.prompt(questions);
-
-
-    switch (results.Action) {
-        case "Optimize":
-            const filename = await filePrompt();
-            console.log(`Optimizing file ${chalk.cyan(filename)}...`);
-            executeOptimizations(filename);
-            break;
-        case "Security Check":
-            await executeSecurityChecks();
-            break;
-        default:
-            console.log("Error");
-            break;
+    if(results.Action === "Cancel") {
+        return;
     }
 
+    const filename = await filePrompt();
+    console.log(`Optimizing file ${chalk.cyan(filename)}...`);
+    executeOptimizations(filename);
 
 }
--- openai.js
+++ openai.js
@@ -7,21 +7,28 @@ export async function generateOptimizedCode(code) {
 
     const openaiKey = fs.readFileSync(`${process.env.HOME}/.checkyourcode`, "utf8").split("=")[1].trim();
     const openai = new OpenAI({ apiKey: openaiKey });
-  try {
-    const completion = await openai.chat.completions.create(
-        {
-            model: "gpt-3.5-turbo",
-            messages: [{ role: "user", content: "Optimize the given code. I want to display this response in command line. So give the optimization description minimum as possible and give the optimised code only instaed of full code for each optimization point. Avoid the starting content and ending content. Just give the points for optimizations.\n"+ code }],
-            max_tokens: 1000,
-            temperature: 0.9,
-        }
-    )
-    
-    const optimizedCode = completion.choices[0].message.content.trim();
-    return optimizedCode;
-  } catch (error) {
-    console.error('Error generating optimized code:', error);
-    throw error;
-  }
+    try {
+        const completion = await openai.chat.completions.create(
+            {
+                model: "gpt-3.5-turbo",
+                messages: [{ role: "user", content: getContent(code) }],
+                max_tokens: 1000,
+                temperature: 0.9,
+            }
+        )
+
+        const optimizedCode = completion.choices[0].message.content.trim();
+        return optimizedCode;
+    } catch (error) {
+        console.error('Error generating optimized code:', error);
+        throw error;
+    }
 }
 
+
+function getContent(code) {
+    return `
+    Act like you are a very experienced software developer and you have deep fundamental knowledge . I have a code file. So give the necessary optimizations that can be done in the code. You should optimize the code as a production ready which considers memory space, time complexity, performance, length of code etc. Give the optimisations points/description followed by the optimized code\n.
+    ${code}`
+
+}
