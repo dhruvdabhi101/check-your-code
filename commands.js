cbc2f5222c35e4a23ed12dbf33565167d6dd204b
Rushi_0508 <rushigandhi14@gmail.com>
2024-01-27 15:59:29 +0530

    feat: security check automation

--- commands.js
+++ commands.js
@@ -1,20 +1,37 @@
+import { exec } from 'child_process';
 import { generateOptimizedCode } from './openai.js';
 import fs from 'fs';
 
 export async function executeOptimizations(file) {
-  console.log(`Optimizing ${file}...`);
+  console.log(`Showing Optimizations in ${file}...`);
 
   // Read the file content or obtain the code you want to optimize
   const codeToOptimize = readFileContent(file);
 
   try {
-    const optimizedCode = await generateOptimizedCode(codeToOptimize);
-    console.log('Optimized Code:', optimizedCode);
+    // For Optimizations
+    // const optimizedCode = await generateOptimizedCode(codeToOptimize);
+    // console.log(optimizedCode);
 
-    // Perform further actions with the optimized code
-    // For example, write the optimized code back to the file or apply it in memory
+    // For Security
+    const snykCommand = 'snyk test';
+    const op = exec(snykCommand, { encoding: 'utf-8' }, (error, stdout) => {
+      if (error) {
+        console.error('Unable to check security. Something went wrong');
+        return;
+      }
+      console.log(stdout);
+    });
+
+    // Wait for the Snyk command to complete
+    await new Promise((resolve) => {
+      op.on('exit', (code) => {
+        console.log(`Security check completed with code ${code}`);
+        resolve();
+      });
+    });
   } catch (error) {
-    console.error('Optimization failed:', error);
+    console.error('Operation failed:', error);
   }
 }
 
